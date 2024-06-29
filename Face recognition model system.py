1c3963de55079e20ef511dc2685193c45fb503bb
NazmiSyed12 <nazmisyed30@gmail.com>
2024-06-29 20:23:53 +0530

    Add files via upload

---
+++ Face recognition model system.py
@@ -0,0 +1,89 @@
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+import cv2
+
+# Specify the path to the folder containing the images
+path = r"C:\Users\Nazmi\OneDrive\Desktop\cvs\ImagesFaceRecognition"
+
+# Initialize lists to store images and their corresponding names
+images = []
+classNames = []
+
+# Get a list of all files in the specified path
+myList = os.listdir(path)
+
+print(myList)
+# Iterate over each file in the list
+for cl in myList:
+    # Check if the file is an image
+    if os.path.isfile(os.path.join(path, cl)) and cl.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tif')):
+        # Read the image using OpenCV
+        curImg = cv2.imread(os.path.join(path, cl))
+        
+        # Append the image and its name to the lists
+        images.append(curImg)
+        classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+     encode = face_recognition.face_encodings(img)[0]
+     encodeList.append(encode)
+    return encodeList
+def markAttendance(name):
+    with open('Attendance.csv','w+') as f:
+        myDataList=f.readlines()
+        nameList=[]
+        for line in myDataList:
+            entry=line.split(',')
+            nameList.append(entry[0])
+        if name not in nameList:
+            now=datetime.now()
+            dtString=now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
+            
+markAttendance('Nazmi Syed')
+encodeListKnown=findEncodings(images)
+print(len(encodeListKnown))
+print('Encoding Complete')
+cap=cv2.VideoCapture(0)
+
+while True:
+    success,img=cap.read()
+    ImgS=cv2.resize(img,(0,0),None,0.25,0.25)
+    ImgS=cv2.cvtColor(ImgS,cv2.COLOR_BGR2RGB)
+    facesCurFrame=face_recognition.face_locations(ImgS)
+    encodesCurFrame = face_recognition.face_encodings(ImgS,facesCurFrame)
+    for encodeFace,faceLoc in zip(encodesCurFrame,facesCurFrame):
+        matches=face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis=face_recognition.face_distance(encodeListKnown,encodeFace)
+        print(faceDis)
+        matchIndex=np.argmin(faceDis)
+        
+        if matches[matchIndex]:
+            name=classNames[matchIndex].upper()
+            print(name)
+            y1,x2,y2,x1=faceLoc
+            y1,x2,y2,x1=y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+            markAttendance(name)
+   # To find the unknown faces we will replace
+
+    if faceDis[matchIndex]< 0.50:
+              name = classNames[matchIndex].upper()
+              markAttendance(name)
+    else: name = 'Unknown'
+#print(name)
+    y1,x2,y2,x1 = faceLoc
+    y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+    cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+    cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+    cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+     #All this does is to check if the distance to our min face is less than 0.5 or not. If its not then this means the person is unknown so we change the name to unknown and don’t mark the attendance
+
+    
